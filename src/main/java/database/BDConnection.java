1ef4abeba5c9dada68752569e0239139a4454f22
Pedro Zanatta <pedrohenriquezanattadeoliveira@gmail.com>
2025-06-14 13:59:08 -0300

    database conection method

--- src/main/java/database/BDConnection.java
+++ src/main/java/database/BDConnection.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-/**
- *
- * @author isadorabaia
- */
 public class BDConnection {
-    
-}
+        public static String driverJDBC = "org.postgresql.Driver";
+        public static String url = "jdbc:postgresql://localhost:5432/database";
+        public static String user = "postgres";
+        public static String senha = "admin";
+
+        public static void main(String[] args) {
+            try {
+                System.out.println("Carregando o driver...");
+                Class.forName(driverJDBC);
+                System.out.println("Driver carregado com sucesso");
+            } catch (Exception e) {
+                System.out.println("Falha no carregamento");
+            }
+
+            try {
+                System.out.println("Conectando ao BD...");
+                Connection conexao = DriverManager.getConnection(url, user, senha);
+                System.out.println("Conexao realizada com sucesso");
+            } catch (Exception e) {
+                System.out.println("Falha na conexao com o BD");
+            }
+        }
+    }
